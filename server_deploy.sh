98e175f038ba1309d8d94d4c44519094d51e5e2d
Giulio Troccoli-Allard <giulio@troccoli.it>
2021-07-11 11:46:12 +0100

    Added deployment script and GitHub Actions

---
+++ server_deploy.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+set -e
+
+echo "Deploying application ..."
+
+# Enter maintenance mode
+(php artisan down --render="errors::503") || true
+
+# Update codebase
+git fetch origin production
+git reset --hard origin/production
+
+# Install dependencies based on lock file
+composer install --no-interaction --no-dev --no-suggest --prefer-dist --optimize-autoloader --no-progress
+
+# Migrate database
+php artisan migrate --force --no-interaction
+
+# Make sure the public disk is available
+php artisan storage:link
+
+# Note: If you're using queue workers, this is the place to restart them.
+# ...
+
+# Clear caches
+php artisan icons:clear --no-interaction
+php artisan event:clear --no-interaction
+php artisan optimize:clear --no-interaction
+
+# Rebuild cache
+php artisan optimize --no-interaction
+php artisan view:cache --no-interaction
+php artisan event:cache --no-interaction
+php artisan icons:cache --no-interaction
+
+# Reload PHP to update opcache
+echo "" | sudo -S service php8.0-fpm reload
+
+# Fix permissions
+chown -R www-data .
+# Exit maintenance mode
+php artisan up
+
+echo "Unfettered Weddings deployed!"
