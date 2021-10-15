93254263f0b693143203d737f68a13874df36eed
Giulio Troccoli-Allard <giulio@troccoli.it>
2021-10-15 16:44:11 +0100

    Fixed logos in the footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,6 +1,8 @@
 <div class="w-full bg-uw-gray text-uw-pink flex flex-col lg:flex-row">
     <div class="lg:w-1/3 mx-auto mt-14 lg:m-auto lg:flex lg:place-content-center">
-        <img src="{{ url('images/logo-square-gray.png') }}" class="w-mobile-logo h-mobile-logo md:w-tablet-logo md:h-tablet-logo lg:w-desktop-logo lg:h-desktop-logo">
+        <img src="{{ url('images/logo-square-gray-mobile-footer.png') }}" alt="Unfettered Weddings logo" class=" md:hidden">
+        <img src="{{ url('images/logo-square-gray-tablet-footer.png') }}" alt="Unfettered Weddings logo" class="hidden md:block lg:hidden">
+        <img src="{{ url('images/logo-square-gray-desktop-footer.png') }}" alt="Unfettered Weddings logo" class="hidden lg:block">
     </div>
     <ul class="lg:w-1/3 lg:m-auto text-center text-base md:text-lg font-normal space-y-5 md:space-y-7">
         <li><a href="#" class="nav-link">About humanism</a></li>
