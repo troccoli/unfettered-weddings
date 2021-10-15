8c6bee9dc608d737baa9151acf3d2fa004648b6a
Giulio Troccoli-Allard <giulio@troccoli.it>
2021-10-15 16:06:20 +0100

    Added hover state for nav bar links

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -3,9 +3,9 @@
         <img src="{{ url('images/logo-square-gray.png') }}" class="w-mobile-logo h-mobile-logo md:w-tablet-logo md:h-tablet-logo lg:w-desktop-logo lg:h-desktop-logo">
     </div>
     <ul class="lg:w-1/3 lg:m-auto text-center text-base md:text-lg font-normal space-y-5 md:space-y-7">
-        <li><a href="#">About humanism</a></li>
-        <li><a href="#">Testimonials</a></li>
-        <li><a href="#">Contact me</a></li>
+        <li><a href="#" class="nav-link">About humanism</a></li>
+        <li><a href="#" class="nav-link">Testimonials</a></li>
+        <li><a href="#" class="nav-link">Contact me</a></li>
     </ul>
     <div class="w-1/2 sm:w-1/3 mx-auto my-14 md:my-16 lg:m-auto flex justify-between lg:justify-center lg:space-x-8 xl:space-x-12">
         <x-social.facebook/>
