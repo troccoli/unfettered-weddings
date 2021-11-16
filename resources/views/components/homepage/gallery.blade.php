a2c9b47e910ed375570e935ccf00c9b24b7f7d60
Giulio Troccoli-Allard <giulio@troccoli.it>
2021-11-16 19:12:47 +0000

    Added gallery photos

--- resources/views/components/homepage/gallery.blade.php
+++ resources/views/components/homepage/gallery.blade.php
@@ -1,34 +1,28 @@
 <x-container class="bg-uw-pink text-uw-gray">
-    <div class="grid-cols-3 px-8  py-12 space-y-10 sm:space-y-20 md:space-y-0 md:grid md:gap-3 md:grid-rows-3">
-        <div class="w-full rounded">
-            <img
-                src="https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8"
-                alt="image">
+    <div
+        class="px-8 grid grid-cols-1 space-y-6 sm:space-y-12 md:grid-cols-2 md:gap-8 md:space-y-0 lg:grid-cols-3 lg:gap-x-12 lg:gap-y-8">
+        <div class="w-full">
+            <img src="{{ url('images/gallery/photo-1.jpg') }}" class="rounded-lg"
+                 alt="man raising his right arm beside woman holding bouquet of flowers">
         </div>
-        <div class="w-full col-span-2 row-span-2 rounded">
-            <img
-                src="https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8"
-                alt="image">
+        <div class="w-full lg:col-span-2 lg:row-span-2">
+            <img src="{{ url('images/gallery/photo-2.jpg') }}" class="rounded-lg"
+                 alt="two women in white and red floral dress">
         </div>
-        <div class="w-full rounded">
-            <img
-                src="https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8"
-                alt="image">
+        <div class="w-full">
+            <img src="{{ url('images/gallery/photo-3.jpg') }}" class="rounded-lg"
+                 alt="red and white eat, drink, and be married signage near brown tree">
         </div>
-        <div class="w-full col-span-2 row-span-2 rounded">
-            <img
-                src="https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8"
-                alt="image">
+        <div class="w-full lg:col-span-2 lg:row-span-2">
+            <img src="{{ url('images/gallery/photo-4.jpg') }}" class="rounded-lg" alt="woman riding on back of man">
         </div>
-        <div class="w-full rounded">
-            <img
-                src="https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8"
-                alt="image">
+        <div class="w-full">
+            <img src="{{ url('images/gallery/photo-5.jpg') }}" class="rounded-lg"
+                 alt="two black steel chairs beside table">
         </div>
-        <div class="w-full rounded">
-            <img
-                src="https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8"
-                alt="image">
+        <div class="w-full">
+            <img src="{{ url('images/gallery/photo-6.jpg') }}" class="rounded-lg"
+                 alt="two man's hands wearing gold-colored wedding rings">
         </div>
     </div>
 </x-container>
